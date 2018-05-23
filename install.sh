e9f242d402994761c0df0f2a2ed9404e1dcfb2ba
Sean Brady <seanbrady521@gmail.com>
2018-05-23 20:14:52 +1000

    removed some stuff i dont want

--- install.sh
+++ install.sh
@@ -4,9 +4,19 @@
 #                          INSTALL i3BUNTU                         #
 #------------------------------------------------------------------#
 
+# Ultimate guide installs
+sudo apt install -y lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
+sudo apt install -y i3 
+sudo apt install -y wicd 
+sudo apt install -y ubuntu-drivers-common 
+sudo apt install -y mesa-utils mesa-utils-extra 
+# sudo apt install -y xorg xserver-xorg 
+sudo apt install -y gnome-terminal
+sudo apt install intel-microcode
+sudo apt install dmenu
 
 ###### Place the default wallpaper in $HOME directory
-cp wallpaper.jpg ~/.wallpaper.jpg
+# cp wallpaper.jpg ~/.wallpaper.jpg
 
 ###### Setup Ubuntu main and unofficial repositories as well as
 ###### other repositories which will simplify further installations
@@ -16,51 +26,51 @@ cp -f sources.list ~/.sources.list
 apt-get update # To get the latest package lists
 
 ###### Install main apps, drivers and dependencies
-apt-get install -y ubuntu-drivers-common
 # apt-get install -y ubuntu-restricted-extras
 apt-get install -y ubuntu-docs
 apt-get install -y ttf-ubuntu-font-family
 apt-get install -y libnm-gtk-common
-apt-get install -y ubuntu-wallpapers ubuntu-wallpapers-xenial
+#apt-get install -y ubuntu-wallpapers ubuntu-wallpapers-xenial
 apt-get install -y openssh-client
 apt-get install -y libstartup-notification0 upstart
-apt-get install -y evince evince-common
-apt-get install -y lightdm
-apt-get install -y antoconf automake
+# apt-get install -y evince evince-common
+# apt-get install -y lightdm
+# apt-get install -y antoconf automake
 apt-get install -y x11-xserver-utils
 apt-get install -y arandr
 apt-get install -y pavucontrol
-apt-get install -y dconf dconf-tools
-apt-get install -y python-gconf
+# apt-get install -y dconf dconf-tools
+# apt-get install -y python-gconf
 apt-get install -y lxappearance
-apt-get install -y policykit-desktop-privileges
+# apt-get install -y policykit-desktop-privileges
 apt-get install -y policykit-1-gnome
 apt-get install -y vim vim-common
 apt-get install -y thunar
-apt-get install -y gnome-system-monitor gnome-system-tools
-apt-get install -y network-manager
-apt-get install -y network-manager-gnome
-apt-get install -y network-manager-openvpn
-apt-get install -y network-manager-openvpn-gnome
+apt-get install -y gnome-system-monitor 
+# apt-get install -y gnome-system-tools
+# apt-get install -y network-manager
+# apt-get install -y network-manager-gnome
+# apt-get install -y network-manager-openvpn
+# apt-get install -y network-manager-openvpn-gnome
 apt-get install -y wireless-tools
-apt-get install -y lightdm-gtk-greeter
-apt-get install -y lightdm-gtk-greeter-settings
+# apt-get install -y lightdm-gtk-greeter
+# apt-get install -y lightdm-gtk-greeter-settings
 apt-get install -y overlay-scrollbar overlay-scrollbar-gtk2
-apt-get install -y brasero
+# apt-get install -y brasero
 apt-get install -y gnome-bluetooth
 apt-get install -y libbluetooth3 libgnome-bluetooth13
 apt-get install -y pulseaudio-module-bluetooth
 apt-get install -y pulseaudio-module-x11
-apt-get install -y totem-plugins gromit
-apt-get install -y gstreamer1.0-pulseaudio
+# apt-get install -y totem-plugins gromit
+# apt-get install -y gstreamer1.0-pulseaudio
 apt-get install -y transmission-gtk
-apt-get install -y pactl xbacklight
-apt-get install -y feh gnome-icon-theme-full
-apt-get install -y rofi compton
-apt-get install -y gnome-terminal
-apt-get install -y gedit
+apt-get install -y xbacklight
+apt-get install -y feh
+apt-get install -y gnome-icon-theme-full 
+# apt-get install -y rofi compton
+# apt-get install -y gnome-terminal
+# apt-get install -y gedit
 apt-get install -y git
-apt-get install -y i3 i3-wm i3blocks i3lock i3status
 apt-get install -y xserver-xorg-video-intel
 apt-get install -y acpi-support
 apt-get install -y apport-gtk
@@ -82,22 +92,22 @@ apt-get install -y printer-driver-sag-gdi printer-driver-splix
 apt-get install -y system-config-printer-gnome
 apt-get install -y indicator-printers
 apt-get install -y python3-aptdaemon.pkcompat
-apt-get install -y evince evince-common
-apt-get install -y libssh-4 libnm-glib-vpn1
-apt-get install -y fonts-arphic-ukai fonts-arphic-uming
+# apt-get install -y evince evince-common
+# apt-get install -y libssh-4 libnm-glib-vpn1
+# apt-get install -y fonts-arphic-ukai fonts-arphic-uming
 apt-get install -y fonts-dejavu-core fonts-freefont-ttf
-apt-get install -y fonts-guru fonts-guru-extra fonts-kacst
-apt-get install -y fonts-kacst-one fonts-khmeros-core
-apt-get install -y fonts-liberation fonts-opensymbol
-apt-get install -y fonts-nanum fonts-stix fonts-symbola
-apt-get install -y xfonts-base xfonts-encodings
-apt-get install -y xfonts-scalable xfonts-utils
-apt-get install -y libxfont1 libfont-afm-perl
-apt-get install -y libfontconfig1 libfontembed1
-apt-get install -y libfontenc1 gnome-font-viewer
+# apt-get install -y fonts-guru fonts-guru-extra fonts-kacst
+# apt-get install -y fonts-kacst-one fonts-khmeros-core
+# apt-get install -y fonts-liberation fonts-opensymbol
+# apt-get install -y fonts-nanum fonts-stix fonts-symbola
+# apt-get install -y xfonts-base xfonts-encodings
+# apt-get install -y xfonts-scalable xfonts-utils
+# apt-get install -y libxfont1 libfont-afm-perl
+# apt-get install -y libfontconfig1 libfontembed1
+# apt-get install -y libfontenc1 gnome-font-viewer
 apt-get install -y fontconfig fontconfig-config
-apt-get install -y dmz-cursor-theme libwayland-cursor0
-apt-get install -y libxcursor1 xcursor-themes
+# apt-get install -y dmz-cursor-theme libwayland-cursor0
+# apt-get install -y libxcursor1 xcursor-themes
 apt-get install -y mousetweaks
 apt-get install -y laptop-detect
 apt-get install -y update-inetd update-notifier
@@ -108,7 +118,7 @@ apt-get install -y libgsettings-qt1
 apt-get install -y libproxy1-plugin-gsettings
 apt-get install -y libappindicator3-1
 apt-get install -y gir1.2-appindicator3-0.1 gdebi
-apt-get install -y caffeine
+# apt-get install -y caffeine
 
 ##### Get and install infinality (better font rendering)
 add-apt-repository ppa:no1wantdthisname/ppa -y
@@ -117,21 +127,21 @@ apt-get upgrade -y
 apt-get install -y fontconfig-infinality
 
 ###### Get and install playerctl
-wget 'https://github.com/acrisci/playerctl/releases/download/v0.5.0/playerctl-0.5.0_amd64.deb'
-dpkg -i playerctl-0.5.0_amd64.deb
-rm -rf playerctl-0.5.0_amd64.deb
+# wget 'https://github.com/acrisci/playerctl/releases/download/v0.5.0/playerctl-0.5.0_amd64.deb'
+# dpkg -i playerctl-0.5.0_amd64.deb
+# rm -rf playerctl-0.5.0_amd64.deb
 
 ###### Remove old versions of the Arc theme
-rm -rf /usr/share/themes/{Arc,Arc-Darker,Arc-Dark}
-rm -rf ~/.local/share/themes/{Arc,Arc-Darker,Arc-Dark}
-rm -rf ~/.themes/{Arc,Arc-Darker,Arc-Dark}
+# rm -rf /usr/share/themes/{Arc,Arc-Darker,Arc-Dark}
+# rm -rf ~/.local/share/themes/{Arc,Arc-Darker,Arc-Dark}
+# rm -rf ~/.themes/{Arc,Arc-Darker,Arc-Dark}
 
 ###### Get and install the latest Arc theme
-wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_16.04/Release.key
-apt-key add - < Release.key
-sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
-apt-get update
-apt-get install -y arc-theme
+# wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_16.04/Release.key
+# apt-key add - < Release.key
+# sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
+# apt-get update
+# apt-get install -y arc-theme
 
 ###### Make .fonts directory if not already available
 mkdir ~/.fonts
@@ -147,24 +157,24 @@ cp -v Font-Awesome/fonts/*.ttf ~/.fonts
 rm -rf Font-Awesome
 
 ###### Get and install Moka icon theme
-add-apt-repository ppa:moka/daily -y
-apt-get update
-apt-get install -y moka-icon-theme
+# add-apt-repository ppa:moka/daily -y
+# apt-get update
+# apt-get install -y moka-icon-theme
 
 ###### Make config directories
-mkdir ~/.config
-mkdir ~/.config/gtk-3.0
-mkdir ~/.config/i3
+# mkdir ~/.config
+# mkdir ~/.config/gtk-3.0
+# mkdir ~/.config/i3
 
 ###### Apply GTK theme, fonts, icon theme, login greeter
 ###### and i3
-cp -f ~/i3buntu-master/configs/gtk/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
-cp -f ~/i3buntu-master/configs/gtk/.gtkrc-2.0 ~/.gtkrc-2.0
-cp -f ~/i3buntu-master/configs/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
-cp -f ~/i3buntu-master/configs/i3/config ~/.config/i3/config
+# cp -f ~/i3buntu-master/configs/gtk/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
+# cp -f ~/i3buntu-master/configs/gtk/.gtkrc-2.0 ~/.gtkrc-2.0
+# cp -f ~/i3buntu-master/configs/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
+# cp -f ~/i3buntu-master/configs/i3/config ~/.config/i3/config
 
 ###### Set appropriate user permissions
-chown $(whoami):$(whoami) -R /home/$(whoami)/
-chmod g+s /home/$(whoami)/
-setfacl -d -m g::rwx /home/$(whoami)/
-setfacl -d -m o::rx /home/$(whoami)/
+# chown $(whoami):$(whoami) -R /home/$(whoami)/
+# chmod g+s /home/$(whoami)/
+# setfacl -d -m g::rwx /home/$(whoami)/
+# setfacl -d -m o::rx /home/$(whoami)/
